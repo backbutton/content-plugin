d7005b0fc567fe951abadb3b38dbba6ebcb5eb13
Wojciech Smoliński <wojsmol@wp.pl>
2017-05-14 12:41:17 +0200

    aplly Joomla CS

--- backbutton.php
+++ backbutton.php
@@ -2,7 +2,7 @@
 // no direct access
 defined('_JEXEC') or die;
 
-jimport( 'joomla.plugin.plugin' );
+jimport('joomla.plugin.plugin');
 
 class plgContentBackButton extends JPlugin
 {
@@ -15,35 +15,37 @@ class plgContentBackButton extends JPlugin
 	 *
 	 * @param object $subject The object to observe
 	 * @param object $params  The object that holds the plugin parameters
+	 *
 	 * @since 1.5
 	 */
-	function plgContentBackbutton( &$subject, $params )
+	function plgContentBackbutton(&$subject, $params)
 	{
-		parent::__construct( $subject, $params );
+		parent::__construct($subject, $params);
 		$this->loadLanguage();
 	}
+
 	/**
 	 * Replaces {backbutton} with a back button link
 	 *
 	 * Method is called by the view
 	 *
-	 * 
-	 * @param	string	The context of the content being passed to the plugin.
-	 * @param	object	The article object.  Note $article->text is also available
-	 * @param	object	The article params
-	 * @param	int		The 'page' number
+	 *
+	 * @param    string     The context of the content being passed to the plugin.
+	 * @param    object     The article object.  Note $article->text is also available
+	 * @param    object     The article params
+	 * @param    int        The 'page' number
 	 *
 	 **/
-	 
-	function onContentPrepare( $context, &$article, &$params, $page=0 )
+
+	function onContentPrepare($context, &$article, &$params, $page = 0)
 	{
 		$app = JFactory::getApplication();
 		$this->params->get('showindiv') == 1 ? $link = '<div class="backbutton">' : $link = '';
 		$this->params->get('linkclass') == 1 ? $aclass = 'class="backbutton"' : $aclass = '';
 		$pattern = '{backbutton}';
-		$link .= '<a href="javascript:history.back();"' . $aclass .'>'. $this->params->get('linklabel', JText::_('BBBACK')) .'</a>';
+		$link    .= '<a href="javascript:history.back();"' . $aclass . '>' . $this->params->get('linklabel', JText::_('BBBACK')) . '</a>';
 		$this->params->get('showindiv') == 1 ? $link .= '</div>' : $link .= '';
 		$article->text = str_replace($pattern, $link, $article->text);
-  }
+	}
 }
-?>
+
